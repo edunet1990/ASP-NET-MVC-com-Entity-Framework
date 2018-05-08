d3de14b7955b22aa5a1b5c783703c6877269a3e2
Eduardo <eduardosilva-9@hotmail.com>
2018-05-08 01:26:58 -0300

    Primeiro e único commit feito para enviar o projeto feito em ASP MVC para o GitHub

---
+++ Mvc_Contatos/Controllers/ContatoController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Contatos.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Mvc_Contatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private CadastroEntities db = new CadastroEntities();
+
+        // GET: Contato
+        public ActionResult Index(string procurarPor, string criterio, int? pagina, string ordenarPor)
+        {
+            ViewBag.OrdenarPorNome = string.IsNullOrEmpty(ordenarPor) ? "Nome desc" : "";
+            ViewBag.OrdenarPorSexo = ordenarPor == "Sexo" ? "Sexo desc" : "Sexo";
+
+            var contatos = db.Contatos.AsQueryable();
+
+            if(procurarPor == "Sexo")
+            {
+                contatos = contatos.Where(x => x.Sexo == criterio || criterio == null);
+            }
+            else
+            {
+                contatos = contatos.Where(x => x.Nome.StartsWith(criterio) || criterio == null); 
+            }
+
+            switch (ordenarPor)
+            {
+                case "Nome desc":
+                    contatos = contatos.OrderByDescending(x => x.Nome);
+                    break;
+
+                case "Sexo desc":
+                    contatos = contatos.OrderByDescending(x => x.Sexo);
+                    break;
+
+                case "Sexo":
+                    contatos = contatos.OrderByDescending(x => x.Sexo);
+                    break;
+
+                default:
+                    contatos = contatos.OrderBy(x => x.Nome);
+                    break;  
+            }
+            return View(contatos.ToPagedList ( pagina ?? 1,5));
+        }
+
+        // GET: Contato/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contato contato = db.Contatos.Find(id);
+            if (contato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contato);
+        }
+
+        // GET: Contato/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contato/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Email,Sexo")] Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contatos.Add(contato);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contato);
+        }
+
+        // GET: Contato/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contato contato = db.Contatos.Find(id);
+            if (contato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contato);
+        }
+
+        // POST: Contato/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Email,Sexo")] Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contato).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contato);
+        }
+
+        // GET: Contato/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contato contato = db.Contatos.Find(id);
+            if (contato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contato);
+        }
+
+        // POST: Contato/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contato contato = db.Contatos.Find(id);
+            db.Contatos.Remove(contato);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Mvc_Contatos/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas através do seguinte
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Mvc_Contatos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mvc_Contatos")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como falso torna não visíveis os tipos neste assembly
+// para componentes COM. Caso precise acessar um tipo neste assembly a partir de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("9c1fc55d-6a12-4483-b9d4-de2a34a1ad41")]
+
+// As informações de versão de um assembly consistem nos seguintes quatro valores:
+//
+//      Versão Principal
+//      Versão Secundária
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou definir como padrão os números de revisão e de versão
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
